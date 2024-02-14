2d1842d747d56b3a1972cec4ee1ef3e1328ef7e9
kakikukekouchi <138739444+kakikukekouchi@users.noreply.github.com>
2024-02-14 16:58:15 +0900

    総合

--- DriveAndAvoid1/Object/Bullet.cpp
+++ DriveAndAvoid1/Object/Bullet.cpp
@@ -2,6 +2,8 @@
 #include "DxLib.h"
 #include <math.h>
 
+int Bullet::tama;
+
 Bullet::Bullet() :location(0.0f), vector(0.0f), speed(0.0f), radius(0.0f), damage(0.0f), type(0)
 {
 }
@@ -13,6 +15,14 @@ Bullet::~Bullet()
 
 void Bullet::Initialize(Vector2D vec, Vector2D loc, float s, float r, float d, int t)
 {
+	if (t == 0) {
+		tama = LoadGraph("Resource/images/yuusyanotama.png");
+	}
+	else
+	{
+		tama= LoadGraph("Resource/images/tekinotama.png");
+	}
+
 	vector = vec;
 	location = loc;
 	speed = s;
@@ -35,7 +45,8 @@ void Bullet::Update()
 
 void Bullet::Draw()
 {
-	DrawCircle(location.x, location.y, radius, 0xff0000, TRUE);
+	//DrawCircle(location.x, location.y, radius, 0xff0000, TRUE);
+	DrawRotaGraphF(location.x, location.y, 1.8, 0, 0, tama, TRUE); 
 }
 
 void Bullet::Finalize()
